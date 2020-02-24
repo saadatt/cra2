5e7e6accce1b34e8ae04e75379fa55f34c358fce
saadatt <saadatt@gmail.com>
2020-02-24 16:41:21 -0600

    Updates

---
+++ precache-manifest.9f28a861582a3e6bd3fb7560c73bfc51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ec085260a58bcccb79cab5da6406ce",
+    "url": "/cra2/index.html"
+  },
+  {
+    "revision": "4b51b615db969e8fc1aa",
+    "url": "/cra2/static/css/main.f5337423.chunk.css"
+  },
+  {
+    "revision": "082076f36972d7dceb45",
+    "url": "/cra2/static/js/2.d6c4c768.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra2/static/js/2.d6c4c768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b51b615db969e8fc1aa",
+    "url": "/cra2/static/js/main.701adeb1.chunk.js"
+  },
+  {
+    "revision": "130af5c5c5c468c9a3c0",
+    "url": "/cra2/static/js/runtime-main.8a268de6.js"
+  }
+]);
